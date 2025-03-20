206628ae98271e5d86b984f196db04f08954a302
bsipek07 <borna.sipek@gmail.com>
2025-03-20 18:46:11 +0100

    prvi commit

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
 
-        String znak;
-
         Scanner input = new Scanner(System.in);
         System.out.println("Unesi rečenicu:");
         String recenica = input.nextLine();
